b15c76f76fec0506da654d79725fb4297cea3d65
Hosam <hossamnasser938@gmail.com>
2019-02-11 21:44:50 +0200

    Render first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log( "Every thing will work I.S.A" );
+import React from "react";
+import { render } from "react-dom";
+
+class AppComponent extends React.Component {
+    render() {
+        return (
+            <div>
+              <h1>Hello, This is the first component</h1>
+            </div>
+        );
+    }
+}
+
+render( <AppComponent/>, window.document.getElementById( "app" ) );
