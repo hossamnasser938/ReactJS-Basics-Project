8e369a400b818db73e6dca793c33f684462a6a57
Hosam <hossamnasser938@gmail.com>
2019-02-11 22:18:34 +0200

    Render multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Home } from './components/Home';
+import { Header } from './components/Header';
+
 class AppComponent extends React.Component {
     render() {
         return (
+          <div>
             <div>
-              <h1>Hello, This is the first component</h1>
+              <Home/>
             </div>
+            <div>
+              <Header/>
+            </div>
+          </div>
         );
     }
 }
