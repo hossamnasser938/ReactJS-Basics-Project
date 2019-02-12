f6bfc525f0eaf691c8424475fd08a5bf09d34c83
Hosam <hossamnasser938@gmail.com>
2019-02-12 17:48:23 +0200

    Output dynamic data: logic _ outside return

--- src/app/index.js
+++ src/app/index.js
@@ -6,12 +6,16 @@ import { Header } from './components/Header';
 
 class AppComponent extends React.Component {
     render() {
-      let x = 5;
-      return (
-          <div>
-            <h1>{ 0 > 0 ? "x is positive": "x is negative" }</h1>
-          </div>
-      );
+        let x = 5;
+        let content = "";
+        if( x > 0 ) {
+            content = "x is positive";
+        } else {
+            content = "x is negative";
+        }
+        return (
+            <h1>{ content }</h1>
+        );
     }
 }
 
