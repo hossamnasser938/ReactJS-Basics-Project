72710f7d07ca04336007fbe8530c3ec999c460c7
Hosam <hossamnasser938@gmail.com>
2019-02-12 17:52:29 +0200

    Output dynamic data: literals

--- src/app/index.js
+++ src/app/index.js
@@ -6,15 +6,8 @@ import { Header } from './components/Header';
 
 class AppComponent extends React.Component {
     render() {
-        let x = 5;
-        let content = "";
-        if( x > 0 ) {
-            content = "x is positive";
-        } else {
-            content = "x is negative";
-        }
         return (
-            <h1>{ content }</h1>
+            <h1>{ "Hello" }</h1>
         );
     }
 }
